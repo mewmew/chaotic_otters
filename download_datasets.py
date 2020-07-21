b41efdeafa67e3655fad92539904b2478bdb6e9f
Robin Eklind <rnd0x00@gmail.com>
2020-07-21 20:51:39 +0200

    add debug output to download_dataset

--- download_datasets.py
+++ download_datasets.py
@@ -2,22 +2,24 @@
 import os, requests
 
 def download_dataset():
-   fname = []
-   for j in range(3):
-      fname.append('steinmetz_part%d.npz'%j)
-      url = ["https://osf.io/agvxh/download"]
-      url.append("https://osf.io/uv3mw/download")
-      url.append("https://osf.io/ehmw2/download")
+	fname = []
+	for j in range(3):
+		fname.append('steinmetz_part%d.npz'%j)
+		url = ["https://osf.io/agvxh/download"]
+		url.append("https://osf.io/uv3mw/download")
+		url.append("https://osf.io/ehmw2/download")
 
-   for j in range(len(url)):
-      if not os.path.isfile(fname[j]):
-         try:
-            r = requests.get(url[j])
-         except requests.ConnectionError:
-            print("!!! Failed to download data !!!")
-         else:
-            if r.status_code != requests.codes.ok:
-               print("!!! Failed to download data !!!")
-            else:
-               with open(fname[j], "wb") as fid:
-                  fid.write(r.content)
+	for j in range(len(url)):
+		print("downloading:", url)
+		if not os.path.isfile(fname[j]):
+			try:
+				r = requests.get(url[j])
+			except requests.ConnectionError:
+				print("!!! Failed to download data !!!")
+			else:
+				if r.status_code != requests.codes.ok:
+					print("!!! Failed to download data !!!")
+				else:
+					with open(fname[j], "wb") as fid:
+						fid.write(r.content)
+	print("download complete")
